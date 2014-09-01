05f08c53f632575384e7ae33974ea3a7f599084c
IvelinStanchev <telerika123@gmail.com>
2014-09-01 10:40:43 +0300

    EntityFramework performance tests

---
+++ EntityFrameworkPerformance/EntityFrameworkPerformance/TelerikAcademy.ConsoleClientTask1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelerikAcademy.Models;
+
+namespace TelerikAcademy.ConsoleClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            TelerikAcademyEntities context = new TelerikAcademyEntities();
+
+            context.Towns.Add(new Town { Name = "Pesho" });
+
+            context.SaveChanges();
+
+            Stopwatch stopWatchWithoutInclude = new Stopwatch();
+            stopWatchWithoutInclude.Start();
+            using (context)
+            {
+                foreach (var employee in context.Employees)
+                {
+                    Console.WriteLine("Name: {0} - Town: {1} - Department: {2}",
+                        employee.FirstName, employee.Address.Town.Name, employee.Department.Name);
+                }
+            }
+
+            stopWatchWithoutInclude.Stop();
+
+            Console.WriteLine(new string('-', 50));
+
+            context = new TelerikAcademyEntities();
+
+            Stopwatch stopWatchWithInclude = new Stopwatch();
+            stopWatchWithInclude.Start();
+
+            using (context)
+            {
+                foreach (var employee in context.Employees.Include("Address").Include("Department"))
+                {
+                    Console.WriteLine("Name: {0} - Town: {1} - Department: {2}",
+                        employee.FirstName, employee.Address.Town.Name, employee.Department.Name);
+                }
+            }
+
+            stopWatchWithInclude.Stop();
+
+            Console.WriteLine("Time without include: {0}", stopWatchWithoutInclude.Elapsed);
+            Console.WriteLine("Time with include:    {0}", stopWatchWithInclude.Elapsed);
+        }
+    }
+}
---
+++ EntityFrameworkPerformance/EntityFrameworkPerformance/TelerikAcademy.ConsoleClientTask2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelerikAcademy.Models;
+
+namespace TelerikAcademy.ConsoleClientTask2
+{
+    class Program
+    {
+        static void Main()
+        {
+            TelerikAcademyEntities context = new TelerikAcademyEntities();
+
+            context.Towns.Add(new Town { Name = "Pesho" });
+
+            context.SaveChanges();
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            //bad performance
+            var allEmployees = context.Employees.ToList().Select(e => e.Address).ToList()
+                .Select(t => t.Town).ToList().Select(t => t.Name == "Sofia");
+
+            Console.WriteLine("The first query ended in:  {0} time", sw.Elapsed);
+
+            sw.Restart();
+
+            //good performance
+            allEmployees = context.Employees.Select(e => e.Address)
+                .Select(t => t.Town).Select(t => t.Name == "Sofia");
+
+            Console.WriteLine("The second query ended in: {0} time", sw.Elapsed);
+        }
+    }
+}
